d3870573ea851a64b42980a20e4170891a63113a
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-02-01 10:35:53 +0300

    1

--- src/main/java/lesson_9/Main.java
+++ src/main/java/lesson_9/Main.java
@@ -1,4 +1,23 @@
 package lesson_9;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//создаем список сотрудников
 public class Main {
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+
+        personList.add(new Person("Dima", 22, Person.Position.ENGINER));//добавляем список с помощью метода add и создаем в нем обьекты
+        personList.add(new Person("Orlov", 62, Person.Position.MANAGER));
+        personList.add(new Person("Ilya", 42, Person.Position.DIRECTOR));
+        personList.add(new Person("Vova", 32, Person.Position.ENGINER));
+        personList.add(new Person("Igor", 29, Person.Position.ENGINER));
+        personList.add(new Person("Dimid", 92, Person.Position.MANAGER));
+
+//выбрать только инженеров и отсортировать по возрасту
+//у потока списка лист можно вызвать фильтр
+
+
+    }
 }
--- src/main/java/lesson_9/Person.java
+++ src/main/java/lesson_9/Person.java
@@ -1,4 +1,52 @@
 package lesson_9;
-//создание списка с помощью функционального программирования
+
+
+//создание списка сотрудников
 public class Person {
+    private String name;
+    private int age;
+    private Position position;
+
+    enum Position {                                           //перечисляемый тип списком
+           ENGINER, MANAGER, DIRECTOR
+    }
+
+    public Person(String name, int age, Position position) {    //конструктор
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", position=" + position +
+                '}';
+    }
 }
