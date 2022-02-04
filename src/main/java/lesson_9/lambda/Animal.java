d645b09f2e2bef2572f8abac8a2b8dc023d1c175
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-02-04 10:54:46 +0300

    1

--- src/main/java/lesson_9/lambda/Animal.java
+++ src/main/java/lesson_9/lambda/Animal.java
@@ -4,4 +4,43 @@ public class Animal {            //в классе два показателя
    private String  name;
    private boolean canJump;
    private boolean cunRun;
+
+    public Animal(String name, boolean canJump, boolean cunRun) {
+        this.name = name;
+        this.canJump = canJump;
+        this.cunRun = cunRun;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCanJump() {
+        return canJump;
+    }
+
+    public void setCanJump(boolean canJump) {
+        this.canJump = canJump;
+    }
+
+    public boolean isCunRun() {
+        return cunRun;
+    }
+
+    public void setCunRun(boolean cunRun) {
+        this.cunRun = cunRun;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", canJump=" + canJump +
+                ", cunRun=" + cunRun +
+                '}';
+    }
 }
