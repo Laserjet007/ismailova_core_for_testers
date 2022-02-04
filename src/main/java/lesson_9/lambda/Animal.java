0b9f0256c5df18cd62b47eeb7791f4d155d9f57c
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-02-04 10:51:52 +0300

    1

--- src/main/java/lesson_9/lambda/Animal.java
+++ src/main/java/lesson_9/lambda/Animal.java
@@ -1,5 +1,7 @@
 package lesson_9.lambda;
 // с помощью функционального программирования отсеявываем нужную информацию в ручную в отличие от предидущего примера с person
 public class Animal {            //в классе два показателя
-
+   private String  name;
+   private boolean canJump;
+   private boolean cunRun;
 }
