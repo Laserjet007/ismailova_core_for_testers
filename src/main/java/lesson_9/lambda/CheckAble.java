f395acf79e390e0cfa54c2c24611253ddbdf68e3
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-02-10 11:31:05 +0300

    1

--- src/main/java/lesson_9/lambda/CheckAble.java
+++ src/main/java/lesson_9/lambda/CheckAble.java
@@ -1,7 +1,7 @@
 package lesson_9.lambda;
 //что бы не делать конструктор в классе CheckIsJumper сделаем интерфейс чековым
 
-public interface CheckAble {
+public interface CheckAble {//для сравнения во стрим ап, необходимо пометить интерфейс тест, что он специальный ... (вариант сравнения №3 из маин)
 
     boolean test(Animal animal);
 }
